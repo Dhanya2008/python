a4692270def27869618a6b4e44f13a5d4591824e
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-28 11:51:04 +0530

    logical

---
+++ operators/logical.py
@@ -0,0 +1,6 @@
+username="DOBBY"
+password=4444
+if username=="DOBBY" or password==444:
+    print("login successful")
+else:
+    print("login failed")
---
+++ operators/logical1.py
@@ -0,0 +1,5 @@
+gender=input("Enter your gender: ")
+if gender=="female":
+    print("You are allowed")
+else:
+    print("You are not allowed")
