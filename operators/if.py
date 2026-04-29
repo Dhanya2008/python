fc1e21368e22ebee61ffc1bfb9d029e61e69bae0
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-04-29 17:02:10 +0530

    if

---
+++ operators/if.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age: "))
+if age<=10:
+    print("ara ticket")
+elif age>=18 and age<60:
+    print("mulu ticket")
+elif age>=60:
+    print("ungaluku free ticket")
+else:
+    print("mulu ticket ")
---
+++ operators/themepark.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age"))
+if age >=18:
+    print("Welcome to wonderla Bangalore")
+    print("Thrilling and water games welcome")
+    gender=input("Enter The gender")
+    if gender =="female" or gender=="male":
+        print("Water games")
+    else:
+        print("Dry games") 
+else:
+    print("GO OUT BYE BYE")
