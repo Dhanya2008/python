cd11f96b64e8f611492c7697873a302790704d4e
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-05-05 11:03:47 +0530

    func1

---
+++ functions/func1.py
@@ -0,0 +1,29 @@
+def Dhanya():
+    print("195+ cutoff")
+Dhanya()
+
+
+#return
+def add():
+    alpha=100
+    beta=100
+    sum=alpha+beta
+    return sum
+print("The sum=",add())
+
+
+
+#without return
+def add():
+    alpha=150
+    beta=500
+    sum=alpha+beta
+    print("The sum",sum)
+add()
+
+#with parameter
+def college(clgname):
+    print("Kiot",clgname)
+college("College")
+    
+
