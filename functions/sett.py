767f789228d3e2d72e2d2e7709e7dd3b9f712492
Dhanya2008 <dhanyashrisenthilkumar45@gmail.com>
2026-05-14 11:20:17 +0530

    sett

---
+++ functions/sett.py
@@ -0,0 +1,20 @@
+name={"aswathi","aishwarya","asmitha","dhanya","asmitha"}
+print(name)
+
+
+#set constructor
+place=set(('salem','nammakal','harry'))
+print(place)
+
+#update
+alpha={'kumar','raja','deepika','satish'}
+alpha.update('mani')
+print(alpha) 
+
+
+#update set 
+trip={'coorg','manali','ooty','shimla'}
+li=['hari']
+trip.update(li)
+print(trip)
+
--- functions/tuplee.py
+++ functions/tuplee.py
@@ -1,30 +1,57 @@
-name=("harry","ron","hermione","neville")
-print(name)
-print(type(name))
+# name=("harry","ron","hermione","neville")
+# print(name)
+# print(type(name))
 
-#slicing
-print(name[0:3])
-print(name[ :2])
-print(name[2: ])
+# #slicing
+# print(name[0:3])
+# print(name[ :2])
+# print(name[2: ])
 
-#tuple method
-num=(10,12,14,16,18,20,12,12)
-numm=num.count(12)
-print(numm)
-inde=num.index(14)
-print(inde)
+# #tuple method
+# num=(10,12,14,16,18,20,12,12)
+# numm=num.count(12)
+# print(numm)
+# inde=num.index(14)
+# print(inde)
 
 
-#append
-college=('psg','kiot','kct','cit')
-collegee=list(college)
-collegee.append('iit')
-college=tuple(collegee)
-print(college)
+# #append
+# college=('psg','kiot','kct','cit')
+# collegee=list(college)
+# collegee.append('iit')
+# college=tuple(collegee)
+# print(college)
 
-#insert
-colors=('blue','green','red','yellow')
-colorss=list(colors)
-colorss.insert(1,'grey')
-colors=tuple(colorss)
-print(colors)
+# #insert
+# colors=('blue','green','red','yellow')
+# colorss=list(colors)
+# colorss.insert(1,'grey')
+# colors=tuple(colorss)
+# print(colors)
+
+#adding tuple to tuple
+# data=('Cat','Dog','dhanya')
+# data1=('aswathi',)
+# data+=data1
+# print(data)
+
+# #loop tuple
+# a=('hi','hello','what')
+# for i in range(len(a)):
+#     print(a[i])
+    
+# for i in a:
+#     print(a)
+    
+    
+#adding
+place=('kerala','telgana','salem')
+place1=(1,2,3)
+result=place+place1
+print(result)
+
+#join
+place=('salem','madurai','chennai')
+join=place*4
+print(join)
+
